0f67db5b02cdd13d7d885d670f9e52acf538571e
dagmawiii03 <dagmawi.yoha03@gmail.com>
2023-04-04 22:47:59 +0300

    fix eslint errors

--- modules/Book.js
+++ modules/Book.js
@@ -1,4 +1,4 @@
-//book class with the determining 3 pararmeters which is the property of any book variable
+// book class with the determining 3 pararmeters which is the property of any book variable
 export default class Book {
   constructor(title, author, id = null) {
     this.title = title;
